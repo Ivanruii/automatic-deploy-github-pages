13c639a37e1d034a86698619051b76b77006834b
Iván Ruiz <ivr.644@gmail.com>
2024-05-13 11:51:21 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <h1>Hello Github Pages</h1>
+      <p>
+        This is just an example of how to do a manual deploy on github pages.
+      </p>
+      <a href="https://docs.github.com/es/pages">Go to github pages docs</a>
+    </main>
+  );
+}
+
+export default App;
